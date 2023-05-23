b18dd2b6322dc3fd7ab77dfc90575316b8102f5d
thaisbreder <thais.breder@zup.com.br>
2023-05-23 19:57:18 -0300

    exercicio 04 finalizado

--- src/E04.java
+++ src/E04.java
@@ -1,2 +1,18 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class E04 {
+    public static void main(String[] args) {
+        Scanner entradaDoUsuario = new Scanner(System.in);
+
+        System.out.println("Digite a sua idade");
+        int idade = entradaDoUsuario.nextInt();
+
+        int meses = idade * 12;
+        int dias = idade * 365;
+
+        System.out.println("A sua idade equivale a " + idade + " anos");
+        System.out.println("A sua idade equivale a " + meses + " meses");
+        System.out.println("A sua idade equivale a " + dias + " dias");
+    }
 }
