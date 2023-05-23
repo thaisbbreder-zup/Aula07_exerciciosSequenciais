a6c85b9a29a5475e62ce022b0c7594b28e670ef5
thaisbreder <thais.breder@zup.com.br>
2023-05-23 18:45:24 -0300

    inclui a condicionais referentes a classificação do IMC

--- src/E02.java
+++ src/E02.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class E02 {
     public static void main(String[] args) {
-        Scanner calculoImc  = new Scanner(System.in);
+        Scanner calculoImc = new Scanner(System.in);
         System.out.println("Digite seu peso em kg");
         double peso = calculoImc.nextDouble();
 
@@ -10,6 +10,18 @@ public class E02 {
         double altura = calculoImc.nextDouble();
 
         double resultadoImc = peso / (altura * altura);
-        System.out.println("O seu IMC é " + resultadoImc);
+        System.out.printf("O seu IMC é " + resultadoImc + ", equivalente a classificação de " );
+
+        if (resultadoImc < 18.5) {
+            System.out.printf("Abaixo do peso. Procure um médico!");
+        } else if
+        (resultadoImc >= 18.5 && resultadoImc <= 24.9) {
+            System.out.printf("Peso normal. Que ótimo! =)");
+        } else if
+        (resultadoImc >= 25.0 && resultadoImc <= 29.9) {
+            System.out.printf("Sobrepeso. Fique atento e procure um médico!");
+        } else if (resultadoImc >= 30.0) {
+            System.out.printf("Obesidade. Procure um médico!");
+        }
     }
 }
